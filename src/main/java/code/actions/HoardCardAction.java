720f664833993f01279504120780b30a18a1968c
Nikki Liu <55368578+LastLightStudios@users.noreply.github.com>
2023-04-02 15:10:59 -0700

    Finished? HoardCardAction

--- src/main/java/code/actions/HoardCardAction.java
+++ src/main/java/code/actions/HoardCardAction.java
@@ -6,12 +6,31 @@ import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.UIStrings;
 
+import static code.ModFile.makeID;
 import static code.util.Wiz.*;
 
+/*
+If I ever want to add card filters, then reference
+https://github.com/kiooeht/StSLib/blob/master/src/main/java/com/evacipated/cardcrawl/mod/stslib/actions/common/SelectCardsInHandAction.java
+ */
+
 public class HoardCardAction extends AbstractGameAction {
-    private AbstractPlayer p = adp();
+    private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString(makeID("Hoard"));
+    private static final AbstractPlayer p = adp();
+
+    public static final int IS_RANDOM = 2;
+    public static final int CAN_PICK_ZERO = 4;
+    public static final int ANY_NUMBER = 8;
+
+    private static int prideGainPerHoardedCard = 1;
+
+    private boolean firstCall = true;
+    private boolean hooksTriggered = false;
+    private boolean completed = false;
 
     // These are for "Hoard this card"
     private AbstractCard playedCard;
@@ -19,50 +38,93 @@ public class HoardCardAction extends AbstractGameAction {
 
     // These are to Hoard other cards
     private boolean isRandom;
-    private boolean anyNumber;
     private boolean canPickZero;
+    private boolean anyNumber;
 
     public HoardCardAction(AbstractCard playedCard) {
         this.playedCard = playedCard;
         hoardThisCard = true;
     }
 
-    public HoardCardAction(int amount, boolean isRandom, boolean anyNumber, boolean canPickZero){
+    public HoardCardAction(int amount, int flags){
         this.amount = amount;
-        this.isRandom = isRandom;
-        this.anyNumber = anyNumber;
-        this.canPickZero = canPickZero;
-    }
-
-    // This will be called when you want to pick a number without a cap
-    public HoardCardAction(boolean anyNumber, boolean canPickZero){
-        this(99, false, anyNumber, canPickZero);
-    }
-
-    // This will be called when you want to pick a number with a cap
-    public HoardCardAction(int amount, boolean anyNumber, boolean canPickZero){
-        this(amount, false, anyNumber, canPickZero);
-    }
-
-    // This is called when you want to Hoard a random number of cards
-    public HoardCardAction(int amount){
-        this(amount, true, false, false);
+        isRandom = (flags & IS_RANDOM) == IS_RANDOM;
+        canPickZero = (flags & CAN_PICK_ZERO) == CAN_PICK_ZERO;
+        anyNumber = (flags & ANY_NUMBER) == ANY_NUMBER;
     }
 
     @Override
     public void update() {
-        if (hoardThisCard) {
-            //probably need to add a check in here to see if its a duplicated card from double tap/burst...b/c I dont think the player would expect those to Hoard
-            applyToSelfTop(new PridePower(adp(), 1));
-            setCardToExhaust(playedCard);
-            this.isDone = true;
-        } else if (isRandom) {
-            for (int i = 0; i < this.amount; i++){
-
-            }
-        } else {
-
+        if (completed){
+            // if (effect.isDone || !AbstractDungeon.topLevelEffects.contains(effect))  where effect is some `private HoardCardEffect effect` I haven't figured out yet
+            isDone = true;
+            // can add some wrap-up/finish method call here if necessary in the future, such as checking for if powers have some type of onHoard effect.
+            return;
         }
+        if (!hooksTriggered){
+            hooksTriggered = true;
+            // this is where stuff that would modify the Hoard effect go, like increasing the amount of Pride gained or something idk...
+            return;
+        }
+        if (firstCall){
+            if (hoardThisCard) {
+                // "Hoard this card." means that none of the other branches apply.
+                //TODO ? add a check in here to see if its a duplicated card from double tap/burst...but maybe not, I think the card would still give Pride, just not end up in Exhaust pile
+                applyToSelfTop(new PridePower(adp(), prideGainPerHoardedCard));
+                setCardToExhaust(playedCard);
+            } else if (p.hand.size() == 0) {
+                // if the hand is empty then there's nothing to Hoard
+            } else if (!anyNumber && p.hand.size() <= amount) {
+                // if the player is required to Hoard more than the remaining cards in their hand
+                amount = p.hand.size();
+                int tmp = p.hand.size();
+                for (int i = 0; i < tmp; i++){
+                    p.hand.moveToExhaustPile(p.hand.getTopCard());
+                }
+                CardCrawlGame.dungeon.checkForPactAchievement();
+                for (int i = 0; i < tmp; i++){
+                    // this happens in a second loop so that all the cards exhaust in a row, then the Pride adds up for each card Hoarded
+                    applyToSelfTop(new PridePower(adp(), prideGainPerHoardedCard));
+                }
+            } else if (isRandom){
+                if (amount > p.hand.size()){
+                    amount = p.hand.size();
+                }
+                for (int i = 0; i < amount; i++){
+                    p.hand.moveToExhaustPile(p.hand.getRandomCard(AbstractDungeon.cardRandomRng));
+                    CardCrawlGame.dungeon.checkForPactAchievement();
+                    applyToSelfTop(new PridePower(adp(), prideGainPerHoardedCard));
+                }
+            } else {
+                // This branch should be for conditions that require the Card Select screen
+                if (amount == 1){
+                    AbstractDungeon.handCardSelectScreen.open(uiStrings.TEXT[1], amount, anyNumber, canPickZero);
+                } else {
+                    AbstractDungeon.handCardSelectScreen.open(uiStrings.TEXT[2], amount, anyNumber, canPickZero);
+                }
+                firstCall = false;
+                return;
+            }
+            completed = true;
+            firstCall = false;
+        } else {
+            // this branch is only reached if firstCall = false && completed = false, so it should only be the branch that opened the Card Select Screen
+            if (!AbstractDungeon.handCardSelectScreen.wereCardsRetrieved){
+                int tmp = AbstractDungeon.handCardSelectScreen.selectedCards.group.size();
+                for (AbstractCard c : AbstractDungeon.handCardSelectScreen.selectedCards.group)
+                    p.hand.moveToExhaustPile(c);
+                CardCrawlGame.dungeon.checkForPactAchievement();
+                for (int i = 0; i < tmp; i++) {
+                    applyToSelfTop(new PridePower(p, prideGainPerHoardedCard));
+                }
+                AbstractDungeon.handCardSelectScreen.wereCardsRetrieved = true;
+                AbstractDungeon.handCardSelectScreen.selectedCards.clear();
+                AbstractDungeon.player.hand.refreshHandLayout();
+                AbstractDungeon.player.hand.applyPowers();
+                completed = true;
+            }
+        }
+        return;
     }
 
     private void setCardToExhaust(AbstractCard card) {
