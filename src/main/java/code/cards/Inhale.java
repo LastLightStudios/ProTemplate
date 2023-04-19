4c9bb92474abb6d8b3a6e46ad4f7fb246f3be203
Nikki Liu <55368578+LastLightStudios@users.noreply.github.com>
2023-04-18 21:51:41 -0700

    Ember support balance changes

--- src/main/java/code/cards/Inhale.java
+++ src/main/java/code/cards/Inhale.java
@@ -1,27 +1,20 @@
 package code.cards;
 
-import basemod.helpers.TooltipInfo;
-import code.actions.ShedAction;
 import code.powers.EmberPower;
-import code.util.DragonUtils;
-import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.watcher.FreeAttackPower;
 
-import java.util.List;
-
 import static code.ModFile.makeID;
 import static code.util.Wiz.applyToSelf;
-import static code.util.Wiz.atb;
 
 public class Inhale extends AbstractEasyCard {
     public final static String ID = makeID("Inhale");
 
     private final static int BLOCK = 8;
     private final static int UPGRADE_BLOCK = 3;
-    private final static int EMBER_GAIN = 1;
-    private final static int UPGRADE_EMBER_GAIN = 1;
+    private final static int EMBER_GAIN = 3;
+    private final static int UPGRADE_EMBER_GAIN = 2;
 
     public Inhale() {
         super(ID, 2, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
